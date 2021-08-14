50008c99b9f780091c4c2d5474922a124f0633ed
Chittaranjan Panda <19926216+pandaind@users.noreply.github.com>
2021-08-14 15:03:17 +0530

    Initial commit

---
+++ src/test/java/com/example/demo/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
